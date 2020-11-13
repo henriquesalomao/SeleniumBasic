849b08248669ee5ae974d5f7ebc19a69b0e94656
unknown <henriquesalomaotinello@gmail.com>
2020-11-12 21:24:18 -0300

    first commit

---
+++ src/Alex.java
@@ -0,0 +1,10 @@
+
+public class Alex {
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+}
+}
---
+++ src/TesteCampoTreinamento.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteCampoTreinamento {
+
+ @Test
+	public void teste() {
+	 System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
+	 WebDriver driver = new ChromeDriver();
+	 
+ }
+}
